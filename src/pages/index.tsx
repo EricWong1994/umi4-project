67708c0df6d88765a9256d35bc69293f1d2c7194
搬砖达人小王 <rang1994@icloud.com>
2022-10-20 18:24:34 +0800

    unocss doesn't work

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,19 @@
 import yayJpg from '../assets/yay.jpg';
+// import 'unocss/dist/index';
+// import '@unocss/cli/s';
 
 export default function HomePage() {
-  return (
-    <div>
-      <h2>Yay! Welcome to umi!</h2>
-      <p>
-        <img src={yayJpg} width="388" />
-      </p>
-      <p>
-        To get started, edit <code>pages/index.tsx</code> and save to reload.
-      </p>
-    </div>
-  );
+	return (
+		<div className='pl-200px bg-blue'>
+			{/* <div className='pl200'> */}
+			<h2>Yay! Welcome to umi!</h2>
+			<p>
+				<img src={yayJpg} width='388' />
+			</p>
+			<p>
+				To get started, edit <code>pages/index.tsx</code> and save to
+				reload.
+			</p>
+		</div>
+	);
 }
